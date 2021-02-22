d0b380dbc96e63250307c079f20d220238b842ba
fetsh-edu <fetsh.me@gmail.com>
2021-02-22 12:16:44 +0300

    Remove lesson-1 changes

--- app/src/main/java/me/fetsh/geekbrains/term_2/android_1/MainActivity.java
+++ app/src/main/java/me/fetsh/geekbrains/term_2/android_1/MainActivity.java
@@ -2,23 +2,14 @@ package me.fetsh.geekbrains.term_2.android_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Intent intentSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        intentSecondActivity = new Intent(this, MainActivity2.class);
-
-        Button button = findViewById(R.id.activity_button);
-        button.setOnClickListener((v) -> startActivity(intentSecondActivity));
-
     }
 }
